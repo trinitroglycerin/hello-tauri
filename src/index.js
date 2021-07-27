0a054a9a0692900ac51bced3092d1d2f4047a065
Dan <dan@aredherring.tech>
2021-07-27 23:58:17 +0100

    add web-to-rust communication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { invoke } from "@tauri-apps/api/tauri";
 
-const App = () => <h1>Hello, world!</h1>;
+const App = () => {
+  const handleClick = async () => {
+    const greeting = await invoke("greet", { name: "Dan" });
+    window.alert(greeting);
+  };
+
+  return (
+    <React.Fragment>
+      <h1>Hello, world!</h1>
+      <button onClick={handleClick}>Click Me!</button>
+    </React.Fragment>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
